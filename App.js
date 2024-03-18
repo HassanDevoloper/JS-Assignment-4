bc4585343c750a8720ffb4f954ac09f17c75eee3
HassanDevoloper <161868607+HassanDevoloper@users.noreply.github.com>
2024-03-18 10:16:20 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+/* Q:1. Declare an empty array using JS literal notation to store
+student names in future. 
+
+              ------Ans-------
+   var studentNames = [];
+   
+ Q:2 Declare an empty array using JS object notation to store
+student names in future.
+   
+              -------Ans-------
+              
+   var studentNames = [];
+   
+ Q:3. Declare and initialize a strings array.
+ 
+              -------Ans-------
+    
+   var studentNames = ["Hassan","Hussain"; "Atique"];
+   
+ Q:4. Declare and initialize a numbers array.
+ 
+              -------Ans-------
+              
+   var num = [10,6,5];
+   
+  Q:5. Declare and initialize a boolean array.
+   
+              -------Ans-------
+              
+   var BooleanArray = [true,false,true];
+   
+   Q:6. Declare and initialize a mixed array.
+  
+   -------Ans-------
+
+var mixedArray = [10,"Hassan",true,false];*/
